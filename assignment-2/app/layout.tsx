95ead833b5f55b4334499868a9718670b10b79cc
Abdul Moiz <abdulmoiz2703@gmail.com>
2025-07-10 23:47:03 +0500

    Created UI using shadcn components

--- assignment-2/app/layout.tsx
+++ assignment-2/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "AI Blog Summarizer",
@@ -23,11 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="{`${poppins.variable} ${roboto.variable}`}">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="bg-white">
+        <main className="bg-purple-950">
           {children}
         </main>
       </body>
