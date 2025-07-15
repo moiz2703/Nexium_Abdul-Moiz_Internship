929fc00981c9840fb14c26288e890fa622ae9307
Abdul Moiz <abdulmoiz2703@gmail.com>
2025-07-15 23:38:51 +0500

    fix: fixed eslint errors fo deployment

--- assignment-2/app/page.tsx
+++ assignment-2/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
